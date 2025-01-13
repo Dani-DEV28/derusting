e8b307fa0a7f48fde573a4cfd2530786e9ebcb2f
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-01-13 10:56:02 -0800

    done list

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,27 +1,56 @@
+import java.text.CollationElementIterator;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class ListPractice {
 
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
 
+    ArrayList<String> firstList = new ArrayList<>();
+
     // Add 3 elements to the list (OK to do one-by-one)
 
+    firstList.add("first");
+    firstList.add("second");
+    firstList.add("third");
+
     // Print the element at index 1
+
+    System.out.println(firstList.get(1));
 
     // Replace the element at index 1 with a new value
     // (Do not insert a new value. The length of the list should not change)
 
+    firstList.set(1,"four");
+
     // Insert a new element at index 0 (the length of the list will change)
 
+    firstList.add(0, "five");
+
     // Check whether the list contains a certain string
+
+    firstList.contains("first");
 
     // Iterate over the list using a traditional for-loop.
     // Print each index and value on a separate line
 
+    for(int i = 0; i < firstList.size()-1; i++){
+      System.out.println(firstList.get(i));
+    }
+
     // Sort the list using the Collections library
+
+    Collections.sort(firstList);
 
     // Iterate over the list using a for-each loop
     // Print each value on a second line
+
+    for(String values: firstList){
+      System.out.println(values);
+    }
 
     /*
      * Usage tip!
